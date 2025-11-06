eaea8f08d0d94cfa3ac993fb45e2bffc1e05c421
Hoang Phuc <phucnguyeho@gmail.com>
2025-11-06 15:22:54 +0700

    ammend the previous because bugs

--- app/api/recommendations.py
+++ app/api/recommendations.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from app.database import get_session
 from app.services import readings as reading_service
-from app.schemas import RecommendItemRequest, RecommendItemResponse
+from app.schemas import RecommendItemRequest, RecommendItemResponse, UserStateRead
 import numpy as np
 from stable_baselines3 import PPO
 
@@ -18,6 +18,12 @@ USER_EMBEDDINGS = {
     "phuc": np.random.randn(OBS_DIM).astype(np.float32),
     "sammy": np.random.randn(OBS_DIM).astype(np.float32),
     "nguye": np.random.randn(OBS_DIM).astype(np.float32),
+}
+
+USER_STATE = {
+    "phuc": UserStateRead(user_id=1, id=1),
+    "sammy": UserStateRead(user_id=2, id=2),
+    "nguye": UserStateRead(user_id=3, id=3),
 }
 
 @router.post("/recommend", response_model=RecommendItemResponse)
@@ -36,5 +42,11 @@ def recommend_api(req: RecommendItemRequest, session: Session = Depends(get_sess
     # update user history
     USER_EMBEDDINGS[req.username] = 0.5 * USER_EMBEDDINGS[req.username] + np.random.randn(OBS_DIM).astype(np.float32)
 
-    return RecommendItemResponse(item=recommended_reading)
+    user_state = USER_STATE[req.username]
+    if user_state.item_ids:
+        user_state.item_ids += f",{item_id}"
+    else:
+        user_state.item_ids = str(item_id)
+
+    return RecommendItemResponse(user_state=user_state, item=recommended_reading)
 
